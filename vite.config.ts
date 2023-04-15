18e294c2cb441e30f536d34ef41ccc7f9220b285
kbario <kylebario1@gmail.com>
2023-04-15 15:41:11 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import solid from "solid-start/vite";
+import static from "solid-start-static";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
-    plugins: [solid({ ssr: true })],
+    plugins: [solid({ adapter: static() })],
   };
 });
