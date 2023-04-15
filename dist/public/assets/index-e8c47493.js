20881384b897e0f5a08742dcb6b92fa985ae48b0
kbario <kylebario1@gmail.com>
2023-04-15 15:18:07 +0800

    built

---
+++ dist/public/assets/index-e8c47493.js
@@ -0,0 +1,1 @@
+import{g as e,t}from"./entry-client-96c9caf4.js";const i=t('<main class="w-screen h-screen flex flex-col gap-8 items-center justify-center"><div>Hello 👋 My name is Kyle.</div><div>I am a web developer.</div><div>I made this in 5 minutes.</div><div>I hope you enjoy the fruits of my labour.'),o=()=>e(i);export{o as default};
