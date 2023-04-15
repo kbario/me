a158fa2f4811678c858c342716cabc619b992b61
kbario <kylebario1@gmail.com>
2023-04-15 08:12:04 +0000

    Deploying to gh-pages from @ kbario/me@18da897dc0ac37c53175ff741859b2548be59d7a 🚀

---
+++ assets/index-11900c80.js
@@ -0,0 +1,1 @@
+import{g as e,t}from"./entry-client-862d11b2.js";const i=t('<main class="w-screen h-screen flex flex-col gap-8 items-center justify-center"><div>Hello 👋 My name is Kyle.</div><div>I am a web developer.</div><div>I made this in 5 minutes.</div><div>I hope you enjoy the fruits of my labour 😁'),o=()=>e(i);export{o as default};
